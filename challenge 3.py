6c555ebb8339fd156922922110df43c0e5bdd10c
santhoshkancharla <santhoshkancharla123@gmail.com>
2026-02-10 22:57:11 +0530

    Refactor student marks evaluation logic

--- challenge 3.py
+++ challenge 3.py
@@ -1,24 +1,27 @@
-list = input("Enter the marks of a student").split()
-count = 0
-count_f = 0
-for z in list:
-    x = int(z)
-    if x>=90 and x<=100:
-        print(x,'-->Excellent')
-        count += 1
-    elif x>=75 and x<=89:
-        print(x,'-->Very Good')
-        count +=1
-    elif x>=60 and x<=74:
-        print(x,'-->Good')
-        count +=1
-    elif x>=40 and x<=59:
-        print(x,'-->Average')
-        count +=1
-    elif x>=0 and x<=39:
-        print(x,'-->Fail')
-        count_f +=1
-    elif x<0 or x>100:
-        print(x,'-->Invalid')
-print("Total valid students :", count)
-print("Total failed students :", count_f)
+n = int(input("enter no of students:"))
+marks = []
+failcount = 0
+valid =n
+for i in range(n):
+
+    marks.append(int(input("enter marks of student ")))
+
+for p in marks:
+    if p>0 and p<=39:
+        print(p,"->fail")
+        failcount = failcount+1
+    elif p>=40 and p<=59:
+        print(p,"->Average")
+    elif p >=60 and p <=74 :
+        print(p,"->Good")
+    elif p >=75 and p <= 89:
+        print(p,"->Very Good")
+    elif p >= 89 and p <= 100:
+        print(p,"->excellent")
+    else :
+        print("Invalid")
+        valid = valid-1
+
+print("Final summary:")
+print("Total valid students:",valid)
+print("Total fail students:",failcount)
